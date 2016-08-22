1cedbddbd7266a1fee33c6bbb2ce167b0d784554
dielduarte <magdiel.kesller@gmail.com>
2016-08-21 22:57:06 -0300

    added a react router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
+
+
 import './assets/css/index.scss';
 
 class Layout extends React.Component {
   render() {
     return (
-      <h1>diel</h1>
+      <h1>Agile notes</h1>
     );
   }
 }
 
-ReactDom.render(<Layout/>, document.getElementById('app'));
+ReactDom.render(<Router history={hashHistory}>
+                  <Route path="/" component={Layout}>
+                     <IndexRoute component={Layout}/>
+                  </Route>
+                </Router>,
+                document.getElementById('app'));
