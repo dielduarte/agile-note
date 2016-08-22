c86f75272db7c11dc4942ad972a62d97f7d0edde
dielduarte <magdiel.kesller@gmail.com>
2016-08-22 00:25:42 -0300

    init login page - design

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, Link, hashHistory, Redirect } from 'react-router';
 
+import { Login } from './pages/login.js';
+import { Main } from './pages/Main.js';
 
-import './assets/css/index.scss';
-
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>Agile notes</h1>
-    );
-  }
-}
+import './assets/css/main.scss';
 
 ReactDom.render(<Router history={hashHistory}>
-                  <Route path="/" component={Layout}>
-                     <IndexRoute component={Layout}/>
+                  <Route name="Login" path="/login" component={Login}>
                   </Route>
+                  <Redirect from="/" to="Login" />
                 </Router>,
                 document.getElementById('app'));
---
+++ src/pages/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Main extends React.Component {
+  render() {
+    return (
+      <h1>Agile notes</h1>
+    );
+  }
+}
